1540da0a4d77726c74f5619b40056126f458db17
Brett Richards <github@bsphere.net>
2019-02-19 10:26:29 +1100

    Updated with whitelist for URLs

--- lithnet.activedirectory.passwordchange.web/Controllers/ChangeController.cs
+++ lithnet.activedirectory.passwordchange.web/Controllers/ChangeController.cs
@@ -3,12 +3,17 @@ using System.Web.Mvc;
 using lithnet.activedirectory.passwordchange.web.Models;
 using System.Threading.Tasks;
 using lithnet.activedirectory.passwordchange.web.Exceptions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace lithnet.activedirectory.passwordchange.web.Controllers
 {
     public class ChangeController : Controller
     {
         private IPasswordManager passwordManager;
+
+        private static List<string> redirectWhitelist;
 
         public ChangeController(IPasswordManager passwordManager)
         {
@@ -26,7 +31,11 @@ namespace lithnet.activedirectory.passwordchange.web.Controllers
 
             if (!String.IsNullOrEmpty(redirect))
             {
-                pageModel.Redirect = Server.HtmlEncode(redirect);
+                redirect = Server.HtmlEncode(redirect);
+                if (ValidRedirectUri(redirect))
+                {
+                    pageModel.Redirect = redirect;
+                }
             }
 
             if (!this.ModelState.IsValid)
@@ -65,6 +74,17 @@ namespace lithnet.activedirectory.passwordchange.web.Controllers
                 await ChangePassword(model.UserName, model.CurrentPassword, model.NewPassword).ConfigureAwait(false);
 
                 model.Success = true;
+                if (model.Redirect != null)
+                {
+                    if (ValidRedirectUri(model.Redirect))
+                    {
+                        return new RedirectResult(model.Redirect);
+                    }
+                    else
+                    {
+                        model.Redirect = null;
+                    }
+                }
                 return this.View("Success", model);
             }
             catch (NotFoundException)
@@ -99,7 +119,14 @@ namespace lithnet.activedirectory.passwordchange.web.Controllers
 
             if (model.Redirect != null)
             {
-                return new RedirectResult(model.Redirect);
+                if (ValidRedirectUri(model.Redirect))
+                {
+                    return new RedirectResult(model.Redirect);
+                }
+                else
+                {
+                    model.Redirect = null;
+                }
             }
 
             return this.View();
@@ -110,6 +137,30 @@ namespace lithnet.activedirectory.passwordchange.web.Controllers
             var userPrincipal = await WindowsSamController.GetUserPrincipal(username);
 
             await WindowsSamController.ChangeUserPassword(userPrincipal, oldPassword, newPassword);
+        }
+
+        public bool ValidRedirectUri(string Uri)
+        {
+            // Only load the whitelist once from file
+            if (redirectWhitelist == null)
+            {
+                redirectWhitelist = System.IO.File.ReadAllLines(Server.MapPath("~/App_Data/RedirectWhitelist.txt")).ToList();
+            }
+
+            if (redirectWhitelist != null)
+            {
+                foreach (var u in redirectWhitelist)
+                {
+                    var pattern = Regex.Escape(u).Replace("%%", ".*");
+                    var result = Regex.Match(Uri.ToLower(), pattern.ToLower());
+                    if (result.Success)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
     }
